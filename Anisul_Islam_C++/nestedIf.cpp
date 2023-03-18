13e776b346c4583ed8d1e9dae93d71db9e2b3002
abdurrahim220 <abdurrahim5@gmail.com>
2023-03-18 11:57:39 +0600

    day2

---
+++ Anisul_Islam_C++/nestedIf.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float mark;
+    cout << "Enter your mark: ";
+    cin >> mark;
+
+    if (mark > 32)
+    {
+        if (mark > 100)
+            cout << "Invalid mark";
+        else if (mark < 100 && mark >= 80)
+            cout << "A+" << endl;
+        else if (mark < 79 && mark >= 70)
+            cout << "A" << endl;
+        else if (mark < 69 && mark >= 60)
+            cout << "A-" << endl;
+        else if (mark < 59 && mark >= 50)
+            cout << "B" << endl;
+        else if (mark < 49 && mark >= 40)
+            cout << "C" << endl;
+        else
+            cout << "D" << endl;
+    }
+    else
+        cout << "Fail";
+}
---
+++ Anisul_Islam_C++/ternaryOperator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, n2;
+    cin >> n1 >> n2;
+
+    int max = (n1 > n2) ? n1 : n2;
+    cout << max;
+}
