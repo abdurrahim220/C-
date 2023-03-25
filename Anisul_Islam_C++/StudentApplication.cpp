f509586c06b45a094a09abbe87914de77d052080
abdurrahim220 <abdurrahim5@gmail.com>
2023-03-25 19:55:43 +0600

    c++

---
+++ Anisul_Islam_C++/StudentApplication.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, sum;
+    cout << "Enter Number of Students: ";
+    cin >> n;
+
+    int students[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Marks for Student " << i + 1 << " = ";
+        cin >> students[i];
+        sum += students[i];
+    }
+
+    cout << "Total Marks = " << sum << endl;
+    float avg = (float)sum / n;
+    cout << "Average: " << avg << endl;
+
+    int max = students[0];
+    int min = students[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (max < students[i])
+        {
+            max = students[i];
+        }
+        if (min > students[i])
+        {
+            min = students[i];
+        }
+    }
+
+    cout << "Maximum marks = " << max << endl;
+    cout << "Minimum marks = " << min << endl;
+}
---
+++ Anisul_Islam_C++/arrayInput.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks[5];
+    for(int i=0;i<=5;i++)
+    {
+        cin>>marks[i];
+    }
+    for(int i=0;i<=5;i++)
+    {
+        cout<<marks[i]<<endl;
+    }
+
+// for(int )
+
+}
